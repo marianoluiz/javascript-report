927c64097ee2b0b16052c6e5517b87ce10691f2e
DaveMagbag <145184152+DaveMagbag@users.noreply.github.com>
2023-12-11 22:29:54 +0800

    Add files via upload

---
+++ report/if-else ladder.js
@@ -0,0 +1,9 @@
+let value = window.prompt("Enter any value");
+
+if (value > 5) {
+    console.log("value is greater than 5");
+} else if (value > 3) {
+    console.log("value is greater than 3");
+} else {
+    console.log("Incorrect!");
+}
